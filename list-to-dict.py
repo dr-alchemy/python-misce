8fee6aa028f827afd0f4be3f4b4aa746dec73d06
Dr Alchemy <you@example.com>
2018-09-09 21:47:21 -0400

    zip command

--- list-to-dict.py
+++ list-to-dict.py
@@ -1,22 +1,32 @@
 import csv
 import json
 
-with open('mylist.txt','rb') as my_file:
+with open('mylist.txt','r') as my_file:
     reader = csv.reader(my_file)
     my_list = list(reader)
 
-print my_list
+# print my_list
 print "\n\n"
 
 data = []
+data2 = []
+
+keys = ['label', 'field', 'data_type', 'default_value']
 
 for x in my_list:
-    temp = {}
-    temp['label'] = x[0]
-    temp['field'] = x[1]
+    # temp = {}
+    # temp['label'] = x[0]
+    # temp['field'] = x[1]
+    # data.append(temp)
+    
+    temp = dict(zip(keys, x))
     temp['data_type'] = ''
     temp['default_value'] = ''
-    
-    data.append(temp)
+    data2.append(temp)
 
-print data
+# print data
+# print json.dumps(data)
+
+print "\n\n"
+
+print json.dumps(data2)
